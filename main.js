7e9dc28c4c18dfd5a5a855d73c5d113a7872cd1f
Nikolay292008 <gulyaevnik29@gmail.com>
2025-11-29 21:02:13 +0300

    добавил 3d анимацию для карточки на главном экране

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     animatedTitles.forEach(title => observer.observe(title));
 });
+
+
+/*3D анимация для карточки на главном экране*/ 
+let home = document.querySelector("#home")
+let descr = document.querySelector(".descr")
+home.addEventListener("mousemove", function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / (window.innerWidth / 2)
+    let angleY = 20 * dy / (window.innerHeight / 2)
+    descr.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
